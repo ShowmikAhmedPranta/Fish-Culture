29879210a57e3a64b5af3c63cf889dd8179bf2b7
ShowmikAhmedPranta <72329588+ShowmikAhmedPranta@users.noreply.github.com>
2020-10-13 02:49:03 +0600

    Update PabdaProject.py
    What's new:
    1. Multi-page interface.
    2. Plain code has been changed to better Object Oriented Programming.
    3. Button 2 has not been changed yet. I will update its action later.

--- PabdaProject.py
+++ PabdaProject.py
@@ -1,34 +1,62 @@
-from tkinter import *
+import tkinter as tk
 import ffiisshh as im
 
-root= Tk()
-btn1 = Button(root,text='About Pabda Fish')
-btn2 = Button(root,text='Cost, Profit Calculation')
-btn4 = Button(root,text='Credits')
-btn1.configure(command=lambda:print("""
-Pabda Fish
---------------------------------------------------------
-Pabda Fish is a fresh water fish of Bangladesh. It is a
-common fish found in river,beel, jheel, haor, etc. It is easily
-cultivable in ponds. It has less fish-bone and is a great
-source of protein. Every age of people usually likes to eat it.
-It has a very little amount of fat.
-Pabda fish is less prone to diseases. Harvesting can be
-done usually within 3 to 6 months. It gains weight between
-30 to 50 grams within 3 to 6 months depending on cultivation.
-"""))
-btn2.configure(command=lambda:im.user_input())
-btn4.configure(command=lambda:print("""
-Credits
+class About(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.label = tk.Label(self, text="""
+            Pabda Fish
+            --------------------------------------------------------
+            Pabda Fish is a fresh water fish of Bangladesh. It is a
+            common fish found in river,beel, jheel, haor, etc. It is easily
+            cultivable in ponds. It has less fish-bone and is a great
+            source of protein. Every age of people usually likes to eat it.
+            It has a very little amount of fat.
+            Pabda fish is less prone to diseases. Harvesting can be
+            done usually within 3 to 6 months. It gains weight between
+            30 to 50 grams within 3 to 6 months depending on cultivation.""")
 
-Showmik Ahmed Pranta
-showmikahmedpranta@gmail.com
+        self.label.pack(padx=100, pady=50)
+        self.btn = tk.Button(self,text="close", command=self.destroy)
+        self.btn.pack(padx=10, pady=5)
 
-Data and literary support by
-Md. Anisur Rahman
-anisurheds@gmail.com"""))
-#btn.config(command=lambda:print("Hello, Tkinter!"))
-btn1.pack(padx=120, pady=30)
-btn2.pack(padx=120, pady=30)
-btn4.pack(padx=120, pady=30)
-root.mainloop()
+class Credits(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.label = tk.Label(self, text="""
+                Credits
+                
+                Showmik Ahmed Pranta
+                showmikahmedpranta@gmail.com
+                
+                Data and literary support by
+                Md. Anisur Rahman
+                anisurheds@gmail.com""")
+        self.label.pack(padx=100, pady= 50)
+        self.btn = tk.Button(self, text="close", command= self.destroy)
+        self.btn.pack(padx=10, pady=5)
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.btn1 = tk.Button(self, text="about Pabda Fish", command=lambda:self.open_about())
+        self.btn2 = tk.Button(self, text="Cost, Profit calculation", command=lambda:im.user_input())
+        self.btn3 = tk.Button(self, text="Credits", command =lambda:self.open_credit())
+        self.btn1.pack()
+        self.btn2.pack()
+        self.btn3.pack()
+
+    def open_about(self):
+        about_window = About(self)
+        about_window.grab_set()
+
+    def open_credit(self):
+        about_credit = Credits(self)
+        about_credit.grab_set()
+
+if(__name__=="__main__"):
+    app = App()
+    app.mainloop()
+
+
+
