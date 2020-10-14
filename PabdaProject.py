9841a959cac1db598b33f979bcf9f6cec66343c9
Showmik Ahmed Pranta <72329588+ShowmikAhmedPranta@users.noreply.github.com>
2020-10-15 04:23:20 +0600

    Update PabdaProject.py
    what's new:
    1.Added complete GUI

--- PabdaProject.py
+++ PabdaProject.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter.constants import TRUE
+ 
+from matplotlib.pyplot import text
 import ffiisshh as im
 
 class About(tk.Toplevel):
@@ -20,6 +23,66 @@ class About(tk.Toplevel):
         self.btn = tk.Button(self,text="close", command=self.destroy)
         self.btn.pack(padx=10, pady=5)
 
+
+class Estimation(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.var = tk.IntVar()
+        self.label = tk.Label(self, text="Enter land area in decimal=")
+        self.entry = tk.Entry(self, textvariable=self.var)
+        self.btn_enter = tk.Button(self, text="Enter", command=lambda:self.action_enter())
+        self.btn_exit = tk.Button(self, text="Exit", command=self.destroy)
+        self.label.pack()
+        self.entry.pack()
+        self.btn_enter.pack()
+        self.btn_exit.pack()
+        
+
+    def action_enter(self):
+        #print("Action Enter")
+        value = self.var.get()
+        #print("value",self.value)
+        self.aaa=im.user_input(value)
+        #print(self.aaa)
+        #print(labell)
+        #self.var1="Hello"
+        self.estimation_text_obj = Estimation_text(self,self.aaa)
+        self.estimation_text_obj.grab_set()
+
+
+
+class Estimation_text(tk.Toplevel):
+    def __init__(self,parent,aab):
+        super().__init__(parent)
+        self.aab = aab
+        #main frame
+        self.main_frame = tk.Frame(self)
+        self.main_frame.pack(fill=tk.BOTH, expand=tk.TRUE,padx=100,pady=50)
+
+        #canvas
+        self.first_canvas = tk.Canvas(self.main_frame)
+        self.first_canvas.pack(side=tk.LEFT, fill= tk.BOTH, expand=1, padx=20, pady=10)
+
+        #Adding a scrollbar to first_canvas
+        self.first_scrollbar = tk.Scrollbar(self.main_frame, orient=tk.VERTICAL, command=self.first_canvas.yview)
+        self.first_scrollbar.pack(side=tk.RIGHT, fill= tk.Y)
+
+        #configuring the canvas
+        self.first_canvas.configure(yscrollcommand=self.first_scrollbar.set)
+        self.first_canvas.bind('<Configure>', lambda e:self.first_canvas.configure(scrollregion=self.first_canvas.bbox("all")))
+
+        #creating another frame inside the canvas
+        self.second_frame = tk.Frame(self.first_canvas)
+
+        #adding that new frame to the window
+        self.first_canvas.create_window((0,0), window= self.second_frame, anchor="nw")
+
+        self.label = tk.Label(self.second_frame, text=self.aab)
+
+        self.label.pack(fill=tk.BOTH)
+
+
+
 class Credits(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
@@ -40,15 +103,22 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.btn1 = tk.Button(self, text="about Pabda Fish", command=lambda:self.open_about())
-        self.btn2 = tk.Button(self, text="Cost, Profit calculation", command=lambda:im.user_input())
+        self.btn2 = tk.Button(self, text="Cost, Profit calculation", command=lambda:self.open_estimation())
         self.btn3 = tk.Button(self, text="Credits", command =lambda:self.open_credit())
-        self.btn1.pack()
-        self.btn2.pack()
-        self.btn3.pack()
+        self.btn4 = tk.Button(self, text="Exit", command=self.destroy)
+        self.btn1.pack(padx=200, pady=25)
+        self.btn2.pack(pady=25)
+        self.btn3.pack(pady=25)
+        self.btn4.pack(pady=25)
 
     def open_about(self):
         about_window = About(self)
         about_window.grab_set()
+
+
+    def open_estimation(self):
+        about_estimation = Estimation(self)
+        about_estimation.grab_set()
 
     def open_credit(self):
         about_credit = Credits(self)
@@ -57,6 +127,3 @@ class App(tk.Tk):
 if(__name__=="__main__"):
     app = App()
     app.mainloop()
-
-
-
